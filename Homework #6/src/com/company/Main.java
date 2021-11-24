360f07be69ae90c9013a27c5f8fb7ee08503ac45
Sheritari <92041560+Sheritari@users.noreply.github.com>
2021-11-25 00:50:30 +0800

    Add files via upload

---
+++ Homework #6/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Homework #6
+ *
+ * @author Sheritari (Severgin Andrey)
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        UsersRepositoryFileImpl newRep = new UsersRepositoryFileImpl("in.txt");
+        User user = newRep.findById(2);
+        System.out.println(user);
+        user.setAge(23);
+        user.setName("Марсель");
+        newRep.update(user);
+        User user2 = newRep.findById(1);
+        user2.setAge(122);
+        newRep.update(user2);
+    }
+}
---
+++ Homework #6/src/com/company/UsersRepositoryFileImpl.java
@@ -0,0 +1,66 @@
+package com.company;
+
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+
+public class UsersRepositoryFileImpl {
+    String file;
+
+    UsersRepositoryFileImpl() {
+    }
+
+    UsersRepositoryFileImpl(String path) {
+        this.file = path;
+    }
+
+    User findById(int id) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            list.add(line);
+        }
+        reader.close();
+        for (String s : list) {
+            if (s.startsWith(id + "|")) {
+                return new User(s);
+            }
+        }
+        throw new IllegalArgumentException("Такого id нет в файле");
+    }
+
+    void update(User user) throws IOException {
+        ArrayList<String> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            list.add(line);
+        }
+        for (String s : list) {
+            if (s.startsWith(user.getId() + "|")) {
+                list.remove(s);
+                list.add(user.toString());
+                Collections.sort(list);
+                break;
+            }
+        }
+        reader.close();
+        try {
+            Files.delete(Path.of(file));
+        } catch (NoSuchFileException x) {
+            System.err.format("%s: no such" + " file or directory%n", file);
+        } catch (DirectoryNotEmptyException x) {
+            System.err.format("%s not empty%n", file);
+        } catch (IOException x) {
+            // File permission problems are caught here.
+            System.err.println(x);
+        }
+        File f = new File(file);
+        FileWriter writer = new FileWriter(f);
+        for (String s : list) {
+            writer.write(s + "\n");
+        }
+        writer.close();
+    }
+}
